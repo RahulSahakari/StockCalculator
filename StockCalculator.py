226319e2ee9a413e140cf0fd889b3a4e282736da
RahulSahakari <rajasahakari@gmail.com>
2021-01-11 16:15:22 +0530

    Added minor changes

--- StockCalculator.py
+++ StockCalculator.py
@@ -1,17 +1,22 @@
 amount = int(input("Amount: "))
-interest = int(input("Interest Rate(Percentage: "))
+interest = int(input("Interest Rate(Percentage): "))
 No_of_years = int(input("No of years: "))
 
-def future(amount,interest,noofyears):
+
+def future(amount, interest, noofyears):
     count = 0
     ans = 0
     while count < noofyears:
-        som = amount * interest / 100
-        # 
-        amount = amount + som
-        gdp = amount * 0.090 / 100
-        amount += gdp
-        count +=1
-        print(amount)
+        # Som is every year the amount of of money added or rather interest
+        som = (amount * interest) / 100
+        # Here im adding interest to the amount
+        amount += som
+        # GDP or rather growth rate or something
+        GDP = amount * 0.492 / 100
+        amount += GDP
 
-future(amount,interest,No_of_years)
+        count += 1
+        print(f"Year No:{count}:{amount}")
+
+
+future(amount, interest, No_of_years)
